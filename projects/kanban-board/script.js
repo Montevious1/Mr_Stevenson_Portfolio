80493c9639c670e465f4d2bad3a1c7e7afd3eaf4
Montevious1 <mstevenson5@yahoo.com>
2026-03-03 21:08:33 -0600

    Create script.js

---
+++ projects/kanban-board/script.js
@@ -0,0 +1,40 @@
+let task = {
+    id: Date.now(),
+    text: input.value,
+    status: "todo",
+    priority: priority
+  };
+
+  tasks.push(task);
+  saveTasks();
+  input.value = "";
+  renderTasks();
+}
+
+function moveTask(id) {
+  let task = tasks.find(t => t.id === id);
+
+  if (task.status === "todo") task.status = "inprogress";
+  else if (task.status === "inprogress") task.status = "done";
+  else task.status = "todo";
+
+  saveTasks();
+  renderTasks();
+}
+
+function renderTasks() {
+  document.getElementById("todo").innerHTML = "";
+  document.getElementById("inprogress").innerHTML = "";
+  document.getElementById("done").innerHTML = "";
+
+  tasks.forEach(task => {
+    let div = document.createElement("div");
+    div.className = "task";
+    div.innerText = task.text + " (" + task.priority + ")";
+    div.onclick = () => moveTask(task.id);
+
+    document.getElementById(task.status).appendChild(div);
+  });
+}
+
+renderTasks();
